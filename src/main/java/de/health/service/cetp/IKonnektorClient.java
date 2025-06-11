6780f4e7030cd3624d93e0965176a0c3a396de60
Alex Kontsur <alexander.kontsur@gmail.com>
2025-06-11 17:24:28 +0400

    EPA-472 * Forward DELETE raw SOAP request to XDS repository EPA-475 * SMC-B handle are not cached anymore * Swagger parameters are added for http headers in Upload/Download endpoints

--- src/main/java/de/health/service/cetp/IKonnektorClient.java
+++ src/main/java/de/health/service/cetp/IKonnektorClient.java
@@ -11,7 +11,6 @@ import de.health.service.config.api.UserRuntimeConfig;
 
 import java.security.cert.X509Certificate;
 import java.util.List;
-import java.util.Map;
 
 @SuppressWarnings("unused")
 public interface IKonnektorClient {
@@ -48,27 +47,4 @@ public interface IKonnektorClient {
     String getKvnr(UserRuntimeConfig userRuntimeConfig, String egkHandle) throws CetpFault;
 
     String getEgkHandle(UserRuntimeConfig userRuntimeConfig, String insurantId) throws CetpFault;
-
-    default <K, V> V computeIfAbsentCetpEx(
-        Map<K, V> map,
-        K key,
-        UserRuntimeConfig userRuntimeConfig,
-        CETPFunction<? super K, ? extends V> mappingFunction
-    ) throws CetpFault {
-        try {
-            return map.computeIfAbsent(key, k -> {
-                try {
-                    return mappingFunction.apply(k, userRuntimeConfig);
-                } catch (Exception e) {
-                    throw new IllegalStateException(e);
-                }
-            });
-        } catch (IllegalStateException e) {
-            if (e.getCause() instanceof CetpFault cetpFault) {
-                throw cetpFault;
-            } else {
-                throw new CetpFault(e.getMessage());
-            }
-        }
-    }
 }
